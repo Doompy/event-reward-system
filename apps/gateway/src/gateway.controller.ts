0e5250e26d79aa3963c45a68d5b5651c1c3780e4
JS\brill <brillarrz@gmail.com>
2025-05-19 21:29:58 +0900

    feature: 퍼블릭 누락 처리 추가

--- apps/gateway/src/gateway.controller.ts
+++ apps/gateway/src/gateway.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Public } from './common/decorators/public.decorator';
 
 @ApiTags('헬스 체크')
 @Controller()
@@ -14,6 +15,7 @@ export class GatewayController {
   @ApiOperation({ summary: '인증 서비스 상태 확인', description: '인증 서비스의 상태를 확인합니다.' })
   @ApiResponse({ status: 200, description: '서비스 상태 정보' })
   @ApiResponse({ status: 500, description: '서비스 연결 실패' })
+  @Public()
   @Get('auth/health')
   async checkAuthHealth() {
     return firstValueFrom(this.authClient.send({ cmd: 'health' }, {}));
@@ -22,6 +24,7 @@ export class GatewayController {
   @ApiOperation({ summary: '이벤트 서비스 상태 확인', description: '이벤트 서비스의 상태를 확인합니다.' })
   @ApiResponse({ status: 200, description: '서비스 상태 정보' })
   @ApiResponse({ status: 500, description: '서비스 연결 실패' })
+  @Public()
   @Get('event/health')
   async checkEventHealth() {
     return firstValueFrom(this.eventClient.send({ cmd: 'health' }, {}));
